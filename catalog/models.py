6482e526010dabd3fde5562c9513019c992661c1
zetmarley <zzzetmarley@gmail.com>
2024-05-06 16:36:15 +0700

    created product and category models with migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, verbose_name='название категории')
+    description = models.TextField(max_length=250, verbose_name='описание категории')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+class Product(models.Model):
+    name = models.CharField(max_length=30, verbose_name='название продукта')
+    description = models.TextField(max_length=250, verbose_name='описание продукта')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение продукта', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField(max_length=10, verbose_name='цена продукта')
+    create_at = models.DateField(auto_now_add=True, verbose_name='дата создания')
+    update_at = models.DateField(auto_now=True, verbose_name='дата изменения')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catalog',
+        'USER': 'postgres',
+        'PASSWORD': '13799731'
     }
 }
 
@@ -123,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
