1e0359a93d8108482e8f56a47c528a74d52f52c2
zetmarley <zzzetmarley@gmail.com>
2024-05-09 16:37:37 +0700

    hm_20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import product_list
+from catalog.views import product_list, product_info
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', product_list, name='product_list'),
+    path('', product_list, name = 'products'),
+    path('product/<int:pk>/', product_info, name = 'product_info')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -4,7 +4,11 @@ from catalog.models import Product
 def product_list(request):
     product_list = Product.objects.all()
 
-    context = {
-        'product_list' : product_list
-    }
+    context = {'product_list' : product_list}
+
     return render(request, 'product_list.html', context)
+
+def product_info(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {'product': product}
+    return render(request, 'product_info.html', context)
