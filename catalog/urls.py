290d77b9df2765af2530375cd4b321073d6fce34
zetmarley <zzzetmarley@gmail.com>
2024-05-10 12:56:24 +0300

    21.1 added CRUD and ViewCounter

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import product_list, product_info
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', product_list, name = 'products'),
-    path('product/<int:pk>/', product_info, name = 'product_info')
+    path('', ProductListView.as_view(), name='products'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_info'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,57 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 from catalog.models import Product
+from django.views.generic.list import ListView
 
-def product_list(request):
-    product_list = Product.objects.all()
 
-    context = {'product_list' : product_list}
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
 
-    return render(request, 'product_list.html', context)
 
-def product_info(request, pk):
-    product = Product.objects.get(pk=pk)
-    context = {'product': product}
-    return render(request, 'product_info.html', context)
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_info.html'
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.product = None
+
+    def get_object(self, queryset=None):
+        self.product = super().get_object(queryset)
+        self.product.views_counter += 1
+        self.product.save()
+        return self.product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'price', 'image', 'category')
+    success_url = reverse_lazy('products:catalog')
+    template_name = 'product_form.html'
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ('name', 'description', 'price', 'image', 'category')
+    success_url = reverse_lazy('products:catalog')
+    template_name = 'product_form.html'
+
+    def get_success_url(self):
+        return reverse('product_info:catalog', args=(self.kwargs.get('pk')))
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('products:catalog')
+    template_name = 'product_confirm_delete.html'
+
+# def product_list(request):
+#     product_list = Product.objects.all()
+#     context = {'product_list': product_list}
+#     return render(request, 'product_list.html', context)
+
+# def product_info(request, pk):
+#     product = Product.objects.get(pk=pk)
+#     context = {'product': product}
+#     return render(request, 'product_info.html', context)
