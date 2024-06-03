892cd0f49e718270aa8028de16cbbc2e587f3ab7
zetmarley <zzzetmarley@gmail.com>
2024-06-03 18:19:07 +0700

    22.2.3

--- users/models.py
+++ users/models.py
@@ -7,8 +7,8 @@ from catalog.models import NULLABLE
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='почта')
-
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
 
     USERNAME_FIELD = 'email'
