7a199880b66aa367d095ca632bb407623cb4d2cc
zetmarley <zzzetmarley@gmail.com>
2024-06-03 17:32:22 +0700

    22.2.1

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'catalog',
-    'materials'
+    'materials',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
---
+++ users/management/commands/csu.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email = 'admin@gmail.com',
+            first_name='admin',
+            last_name='admin',
+            is_staff = True,
+            is_superuser = True,
+        )
+
+        user.set_password('13799731')
+        user.save()
