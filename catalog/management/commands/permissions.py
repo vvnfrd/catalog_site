0f1853b018928dfe1c748074ba5cea38f3b62c69
zetmarley <zzzetmarley@gmail.com>
2024-06-15 16:17:19 +0700

    Try 23.1 #2

--- catalog/management/commands/permissions.py
+++ catalog/management/commands/permissions.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import get_object_or_404
+
 from users.models import User
 
+user = get_object_or_404(User)
 content_type = ContentType.objects.get_for_model(User)
 # получить запись о необходимых правах доступа
 permission = Permission.objects.get(
-    codename="change_blogpost",
+    codename="change_description",
     content_type=content_type,
 )
 # и установить права для пользователя
